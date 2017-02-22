d6a26fe5fad5c75651620c38254f6849b1b619de
Akshay Goradia <asgoradia@gmail.com>
2017-02-22 01:27:18 -0500

    Added article views and controllers

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :photos 
+<<<<<<< HEAD
   has_many :articles
+=======
+>>>>>>> recovery
   
   scope :active, where('active = ?', true)
   scope :alphabetical, order('name')
